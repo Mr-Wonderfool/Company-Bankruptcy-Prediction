9718b36f37294773669b9ae82492da5be977eb68
Wonderful <2251804@tongji.edu.cn>
2024-07-10 22:46:32 +0800

    finished

--- models/utils/RandomForestSelector.py
+++ models/utils/RandomForestSelector.py
@@ -3,7 +3,7 @@ from sklearn.ensemble import RandomForestClassifier
 import matplotlib.pyplot as plt
 
 class RandomForestSelector:
-    def __init__(self,X,Y,trees=11):
+    def __init__(self,X,Y,trees=10):
         self.X=X
         self.Y=Y
         self.trees=trees
@@ -14,11 +14,14 @@ class RandomForestSelector:
         self.indices = np.argsort(self.importances)[::-1]
         self.std = np.std([tree.feature_importances_ for tree in dtree.estimators_], axis=0)
         return self.indices
-    def plot(self, figsize):
-        plt.figure(figsize=figsize)
-        plt.title("Feature importances")
-        plt.bar(range(self.X.shape[1]), self.importances[self.indices],
-            color="r", yerr=self.std[self.indices], align="center")
-        plt.xticks(range(self.X.shape[1]), self.indices)
-        plt.xlim([-1, self.X.shape[1]])
-        plt.show()
+    def plot(self, columns, figsize=(15,6), threshold=.2):
+        fig = plt.figure(figsize=figsize)
+        ax = fig.add_subplot(111)
+        ax.set_title(f"Feature Importances with Random Forest")
+        filter_ = np.count_nonzero(self.importances > threshold)
+        X_indices = range(filter_)
+        ax.bar(X_indices, self.importances[self.indices][:filter_], width=.5,
+            color="b", yerr=self.std[self.indices][:filter_], align="center")
+        plt.xticks(X_indices, self.indices[:filter_])
+        ax.set_xticklabels(columns[self.indices[:filter_]], rotation=30, fontsize='small', ha='right')
+        return fig, ax
